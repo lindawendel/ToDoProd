eea81227e07e29e4b18aaa25d3a9d3127920dd82
Linda Wendel <linda.m.wendel@gmail.com>
2023-12-15 15:13:34 +0100

    trying to work with localDB

---
+++ ToDoProd/ToDoTest/AddToDoTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoCore.Controllers;
+using ToDoCore.Models;
+
+namespace ToDoTest
+{
+    public class AddToDoTest : IClassFixture<TestDatabaseFixture>
+    {
+        //REQUIREMENTS
+        // when data is retrieved from db, we expect that
+        // all details included
+
+        public AddToDoTest(TestDatabaseFixture fixture)
+        => Fixture = fixture;
+
+        public TestDatabaseFixture Fixture { get; }
+    
+
+        [Fact]
+        public void Should_Return_New_Todo_With_Details()
+        {
+            // ARRANGE
+
+            using var context = Fixture.CreateContext();
+            var toDoController = new ApiController(context);
+
+            var newToDo = new ToDoNote
+            {
+                Id = 22,
+                Text = "TestToDo",
+                IsDone = false,
+            };
+
+
+
+            //var blog = controller.GetBlog("Blog2").Value;
+
+            //Assert.Equal("http://blog2.com", blog.Url);
+
+            //processors to handle the post
+            //var processor = new AddToDoTestProcessor();
+
+
+            //ACT 
+            //method for processing the request
+            toDoController.PostToDo(newToDo);
+            Assert.Equal(22, toDoController.GetNote(newToDo.Id).Id);
+
+            //AddToDoResult
+
+        }
+
+    }
+}
---
+++ ToDoProd/ToDoTest/TestDataBaseFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoCore.Data;
+using ToDoCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoTest
+{
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=ToDoNotes;Trusted_Connection=True";
+            //@"Server=(localdb)\mssqllocaldb;Database=EFTestSample;Trusted_Connection=True";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                        context.AddRange(
+                            new ToDoNote { Id = 1, Text = "Milk", IsDone = false },
+                            new ToDoNote { Id = 1, Text = "Butter", IsDone = false },
+                            new ToDoNote { Id = 1, Text = "Bread", IsDone = true},
+                            new ToDoNote { Id = 1, Text = "Tomatoes", IsDone = false });
+                        context.SaveChanges();
+                    }
+                   
+                }
+
+                    _databaseInitialized = true;
+                }
+            }
+        public ApiContext CreateContext()
+           => new ApiContext(
+               new DbContextOptionsBuilder<ApiContext>()
+                   .UseSqlServer(ConnectionString)
+                   .Options);
+    }
+
+}
