4a22de9c762dbcbf31b2f71a83dd89b67d6da952
Linda Wendel <linda.m.wendel@gmail.com>
2023-12-20 12:08:02 +0100

    initiated test

---
+++ ToDoProd/ToDoTest/ShowToDos_Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoCore.Controllers;
+
+namespace ToDoTest
+{
+    public class ShowToDos_Test : IClassFixture<TestDatabaseFixture>
+    {
+        public ShowToDos_Test(TestDatabaseFixture fixture)
+       => Fixture = fixture;
+
+        public TestDatabaseFixture Fixture { get; }
+
+        [Fact]
+        public void Should_Return_All_ToDos_From_Db() 
+        {
+
+            //ARRANGE
+
+
+            //Initializes DB with four notes(ToDos)
+            using var context = Fixture.CreateContext();
+            var toDoController = new ApiController(context);
+
+
+            //ACT
+
+
+
+
+
+
+
+            //ASSERT
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
